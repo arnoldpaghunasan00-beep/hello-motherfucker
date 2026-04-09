c7edb62b31bfcd50df5b67b6d25da7381ab4fe6d
arnold <arnoldpaghunasan00@gmail.com>
2026-04-09 20:33:07 +0800

    initial commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('public/uploads'));
 
 // ✅ multer setup (image upload)
 const storage = multer.diskStorage({
